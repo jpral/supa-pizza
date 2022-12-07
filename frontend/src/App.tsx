5017617f75d225dd0bb2ccd21b4c82f5ef7677ff
Javi Pral <jpral@me.com>
2022-12-07 11:41:55 +0100

    Style: Add basic structure for header and hero

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { EuiFlexGroup, EuiFlexItem, EuiPage, EuiPanel } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPage, EuiPanel, EuiTitle, useEuiFontSize, useEuiTheme } from '@elastic/eui';
 
 import { Header } from './components/Header';
 import { Trend } from './components/Trend'
@@ -6,13 +6,44 @@ import { OrderRatio } from './components/OrderRatio';
 import { HappiestBelly } from './components/HappiestBelly';
 import { PopularIngredients } from './components/PopularIngredients';
 import { OrderGrid } from './components/OrderGrid';
+import { css } from '@emotion/react';
+import { useCustomStyle } from './hooks/useCustomStyle';
 
 export const App = () => {
+  const { chartColors } = useCustomStyle();
+  const { euiTheme } = useEuiTheme();
+
+  const greetingStyle = css`
+    padding: ${euiTheme.size.l};
+    ${useEuiFontSize('xl')};
+    color: ${euiTheme.colors.lightestShade};
+    font-weight: ${euiTheme.font.weight.medium};
+    letter-spacing: -0.02em;
+  `
+  const heroStyle = css`
+    margin-top: 10px;
+    margin-bottom: 20px;
+    width: 100%;
+  `
+
+  const pageStyle = css`
+    background-image: linear-gradient(45deg, ${chartColors[0]}, ${chartColors[1]});
+    background-position: top center;
+    background-size: 100% 200px;
+    background-repeat: no-repeat;
+    padding: ${euiTheme.size.l};
+  `
+
   return (<>
     <Header />
-    <EuiPage paddingSize='m'>
-
+    <EuiPage css={pageStyle}>
       <EuiFlexGroup wrap style={{ width: '100%' }}>
+        <EuiFlexItem css={heroStyle}>
+          <EuiTitle css={greetingStyle}>
+            <h3>Welcome back, Owner
+            </h3>
+          </EuiTitle>
+        </EuiFlexItem>
         <EuiFlexItem style={{ minWidth: '30%' }}>
           <EuiPanel hasShadow={false} hasBorder={true}>
             <OrderRatio />
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,17 @@
-import { EuiHeader } from '@elastic/eui'
-
+import { EuiAvatar, EuiHeader, EuiHeaderLogo, EuiHeaderSection, EuiHeaderSectionItem } from '@elastic/eui'
+import PizzaLogo from '../assets/logo.svg'
 export const Header = () => {
-  return <EuiHeader theme='dark' />;
+  return <EuiHeader theme='dark'>
+    <EuiHeaderSection>
+      <EuiHeaderSectionItem>
+        <EuiHeaderLogo iconType={PizzaLogo}>Pi • za</EuiHeaderLogo>
+      </EuiHeaderSectionItem>
+    </EuiHeaderSection>
+    <EuiHeaderSection side="right">
+      <EuiHeaderSectionItem css={{ gap: '10px', color: 'white' }}>
+        <span>Proud Owner</span>
+        <EuiAvatar name='Proud Owner' />
+      </EuiHeaderSectionItem>
+    </EuiHeaderSection>
+  </EuiHeader>;
 }
